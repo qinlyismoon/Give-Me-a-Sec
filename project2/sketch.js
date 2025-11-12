62e2c853fd569427d5effdd123060703b1baf710
qinlyismoon <phoebeqincreative@gmail.com>
2025-11-12 14:41:11 -0500

    Update sketch.js

--- project2/sketch.js
+++ project2/sketch.js
@@ -84,7 +84,7 @@ function draw() {
     if (isMouseInBounds(disinfo.bounds)) {
       disinfo.x += random(-5, 5);
       disinfo.y += random(-5, 5);
-      fill("#ab2734");
+      fill("#BF1A1A");
     } else {
       fill(0);
     }
@@ -136,7 +136,7 @@ function updateAndDrawFragments(disinfo) {
     frag.y += frag.vy;
     frag.vy += 0.15;
     frag.alpha -= 4;
-    fill(171, 39, 52, frag.alpha);
+    fill(191, 26, 26, frag.alpha);
     text(frag.char, frag.x, frag.y);
   }
 }
